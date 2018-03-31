7312d870b02813db3612d65c116c4dd5cc103d81
Your Name <tina.yuting.lee@gmail.com>
2018-03-31 18:56:42 +0000

    Restricted lesson viewing to enrolled users

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course
   def show
   end
 
@@ -7,5 +9,11 @@ private
 helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+end
+
+def require_enrolled_for_current_course
+  if current_user.enrolled_in?(current_lesson.section.course) == false
+    redirect_to course_path(current_lesson.section.course), alert: 'Must be enrolled'
   end
 end
